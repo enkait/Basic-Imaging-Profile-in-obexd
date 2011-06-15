8b185d3d111eaf018447eee6298fa94b8b28e4fa
Jakub Adamek <jakub.adamek@uj.edu.pl>
2011-06-15 23:56:50 +0200

    Experimental commit.

--- plugins/image_pull.h
+++ plugins/image_pull.h
@@ -21,14 +21,27 @@
  *
  */
 
+struct pull_aparam_field {
+    uint16_t nbreturnedhandles;
+    uint16_t liststartoffset;
+    uint8_t latestcapturedimages;
+};
+
+struct pull_aparam_header {
+	uint8_t tag;
+	uint8_t len;
+	uint8_t val[0];
+} __attribute__ ((packed));
+
+struct image_pull_session {
+    struct obex_session *os;
+    struct pull_aparam_field *aparam;
+};
+
 void *image_pull_connect(struct obex_session *os, int *err);
 int image_pull_get(struct obex_session *os, obex_object_t *obj, gboolean *stream,
 							void *user_data);
 int image_pull_chkput(struct obex_session *os, void *user_data);
 int image_pull_put(struct obex_session *os, obex_object_t *obj, void *user_data);
 void image_pull_disconnect(struct obex_session *os, void *user_data);
-
-struct image_pull_session {
-    struct obex_session *os;
-};
 
