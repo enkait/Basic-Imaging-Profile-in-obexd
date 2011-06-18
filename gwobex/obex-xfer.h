1c65d493c5967da004f274015b616a4b924aeeb8
Jakub Adamek <adamek.kuba@gmail.com>
2011-06-18 03:47:11 +0200

    Convenience method to make deep copy of a_header

--- gwobex/obex-xfer.h
+++ gwobex/obex-xfer.h
@@ -96,6 +96,8 @@ void a_header_free(struct a_header *ah);
 struct a_header *make_a_header(uint8_t hi, obex_headerdata_t hv,
                                unsigned int hlen);
 
+struct a_header *a_header_copy(struct a_header *ah);
+
 struct gw_obex_xfer *gw_obex_xfer_new(struct gw_obex *ctx, gboolean async, int stream_fd);
 
 void _gw_obex_xfer_free(struct gw_obex_xfer *xfer);
