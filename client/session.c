e63024c178145dc9be4c65a534598f41ecf8b469
Jakub Adamek <adamek.kuba@gmail.com>
2011-06-21 20:53:35 +0200

    Test modification.
    This introduces get/put w aheaders and apparams.
    Should work, but not certain.

--- client/session.c
+++ client/session.c
@@ -1419,6 +1419,60 @@ static void session_prepare_get(struct session_data *session,
 	DBG("Transfer(%p) started", transfer);
 }
 
+int session_get_with_aheaders(struct session_data *session, const char *type,
+		const char *filename, const char *targetname,
+		const guint8 *apparam, gint apparam_size,
+        const GSList * aheaders,
+		session_callback_t func)
+{
+	struct transfer_data *transfer;
+	struct transfer_params *params = NULL;
+    GSList *aheaders_copy = NULL;
+	int err;
+
+	if (session->obex == NULL)
+		return -ENOTCONN;
+
+	if (apparam != NULL) {
+		params = g_new0(struct transfer_params, 1);
+		params->data = g_new(guint8, apparam_size);
+		memcpy(params->data, apparam, apparam_size);
+		params->size = apparam_size;
+	}
+    
+    if (aheaders) {
+        const GSList *aheaders_src = aheaders;
+        while (aheaders_src) {
+            aheaders_copy = g_slist_append(aheaders_copy,
+                    a_header_copy(aheaders_src->data));
+            aheaders_src = g_slist_next(aheaders_src);
+        }
+    }
+
+	transfer = transfer_register(session, filename, targetname, type,
+					params, aheaders_copy);
+	if (transfer == NULL) {
+		if (params != NULL) {
+			g_free(params->data);
+			g_free(params);
+		}
+		return -EIO;
+	}
+
+	if (func != NULL) {
+		struct session_callback *callback;
+		callback = g_new0(struct session_callback, 1);
+		callback->func = func;
+		session->callback = callback;
+	}
+
+	err = session_request(session, session_prepare_get, transfer);
+	if (err < 0)
+		return err;
+
+	return 0;
+}
+
 int session_get(struct session_data *session, const char *type,
 		const char *filename, const char *targetname,
 		const guint8 *apparam, gint apparam_size,
@@ -1826,10 +1880,12 @@ int session_put(struct session_data *session, char *buf, const char *targetname)
 
 int session_put_with_aheaders(struct session_data *session, const char *type,
 		const char *filename, const char *targetname,
+		const guint8 *apparam, gint apparam_size,
 		const GSList *aheaders,
 		session_callback_t func)
 {
 	struct transfer_data *transfer;
+	struct transfer_params *params = NULL;
     GSList *aheaders_copy = NULL;
     int err;
 
@@ -1838,6 +1894,13 @@ int session_put_with_aheaders(struct session_data *session, const char *type,
 
 	if (session->pending != NULL)
 		return -EISCONN;
+	
+    if (apparam != NULL) {
+		params = g_new0(struct transfer_params, 1);
+		params->data = g_new(guint8, apparam_size);
+		memcpy(params->data, apparam, apparam_size);
+		params->size = apparam_size;
+	}
 
     if (aheaders) {
         const GSList *aheaders_src = aheaders;
@@ -1848,7 +1911,8 @@ int session_put_with_aheaders(struct session_data *session, const char *type,
         }
     }
 
-	transfer = transfer_register(session, filename, targetname, type, NULL, aheaders_copy);
+	transfer = transfer_register(session, filename, targetname, type, params,
+                                                              aheaders_copy);
 	if (transfer == NULL) {
         while(aheaders_copy) {
             a_header_free(aheaders_copy->data);
