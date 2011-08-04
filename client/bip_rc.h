ff375dfe8f6cb676d5ecdbdf36c7c0dfc622bb37
Jakub Adamek <adamek.kuba@gmail.com>
2011-08-04 18:29:49 +0200

    Bip rc: added store flag define

--- client/bip_rc.h
+++ client/bip_rc.h
@@ -3,5 +3,8 @@
 #define IMG_HANDLE_HDR (OBEX_HDR_TYPE_BYTES | 0x30)
 #define IMG_DESC_HDR (OBEX_HDR_TYPE_BYTES | 0x71)
 
+#define STOREFLAG_TAG 0x0A
+#define STOREFLAG_LEN 0x01
+
 extern GDBusMethodTable remote_camera_methods[];
 extern GDBusSignalTable remote_camera_signals[];
