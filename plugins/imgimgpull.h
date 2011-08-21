63b6cfba7ece7100ca82fd1e9ca14cff36176257
Jakub Adamek <adamek.kuba@gmail.com>
2011-08-21 16:57:20 +0200

    Imgimgpull: free in close, add debug

--- plugins/imgimgpull.h
+++ plugins/imgimgpull.h
@@ -9,6 +9,18 @@ struct img_desc {
 	gboolean recv_enc, recv_pixel;
 };
 
+struct imgimgpull_data {
+	void *context;
+	char * (*get_image_path) (void *context, int handle);
+	int fd, handle;
+	size_t size;
+	gboolean size_sent, write;
+	struct img_desc *desc;
+};
+
+void free_img_desc(struct img_desc *desc);
+void free_imgimgpull_data(struct imgimgpull_data *data);
+
 gboolean img_elem_attr(struct img_desc *desc, const gchar *key,
 					const gchar *value, GError **gerr);
 void img_elem(GMarkupParseContext *ctxt, const gchar *element,
